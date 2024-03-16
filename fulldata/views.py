163f83b7ea194fd025938784dabcc03db660df0a
Manish Gautam <itsmanishgautam@gmail.com>
2024-03-16 23:01:58 +0545

    Basic Built

---
+++ fulldata/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def myCv(request):
+    return render(request, 'myCv.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
